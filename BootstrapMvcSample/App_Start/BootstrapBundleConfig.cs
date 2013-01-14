2b98e5d12bba335189c4851fc3afed9c5e28409e
John Reilly <johnny_reilly@hotmail.com>
2013-01-14 12:40:30 +0000

    Tweaks

--- BootstrapMvcSample/App_Start/BootstrapBundleConfig.cs
+++ BootstrapMvcSample/App_Start/BootstrapBundleConfig.cs
@@ -14,21 +14,21 @@ namespace BootstrapSupport
         {
             bundles.Add(new ScriptBundle("~/js").Include(
                 "~/Scripts/jquery-*",
-                "~/Scripts/globalize.js",
+                "~/Scripts/globalize.js", //The Globalize library
                 "~/Scripts/bootstrap.js",
-                "~/Scripts/bootstrap-datepicker.js",
+                "~/Scripts/bootstrap-datepicker.js", //This is the brand new internationalised Bootstrap Datepicker 
                 "~/Scripts/jquery.validate.js",
                 "~/Scripts/jquery.validate.unobtrusive.js",
                 "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js",
-                "~/Scripts/jquery.validate.globalize.js"
+                "~/Scripts/jquery.validate.globalize.js" //My jQuery Validate extension which depends on Globalize
                 ));
 
-            //Create culture specific bundles
+            //Create culture specific bundles which contain the JavaScript files that should be served for each culture
             foreach (var culture in CultureInfo.GetCultures(CultureTypes.AllCultures))
             {
-                bundles.Add(new ScriptBundle("~/js-culture." + culture.Name).Include(
-                    DetermineCultureFile(culture, "~/Scripts/globalize-cultures/globalize.culture.{0}.js"),
-                    DetermineCultureFile(culture, "~/Scripts/bootstrap-datepicker-locales/bootstrap-datepicker.{0}.js")
+                bundles.Add(new ScriptBundle("~/js-culture." + culture.Name).Include( //example bundle name would be "~/js-culture.en-GB"
+                    DetermineCultureFile(culture, "~/Scripts/globalize-cultures/globalize.culture.{0}.js"),             //The Globalize locale-specific JavaScript file
+                    DetermineCultureFile(culture, "~/Scripts/bootstrap-datepicker-locales/bootstrap-datepicker.{0}.js") //The Bootstrap Datepicker locale-specific JavaScript file
                 ));
             }
 
@@ -46,12 +46,12 @@ namespace BootstrapSupport
         /// Given the supplied culture, determine the most appropriate Globalize culture script file that should be served up
         /// </summary>
         /// <param name="culture"></param>
-        /// <param name="filePattern"></param>
-        /// <param name="defaultCulture">Default localisation to use</param>
+        /// <param name="filePattern">a file pattern, eg "~/Scripts/globalize-cultures/globalize.culture.{0}.js"</param>
+        /// <param name="defaultCulture">Default culture string to use (eg "en-GB") if one cannot be found for the supplied culture</param>
         /// <returns></returns>
-        private static string DetermineCultureFile(CultureInfo culture, 
+        private static string DetermineCultureFile(CultureInfo culture,
             string filePattern,
-            string defaultCulture = "en-GB" // I'm a Brit and this is mine
+            string defaultCulture = "en-GB" // I'm a Brit and this is my default
             )
         {
             //Determine culture - GUI culture for preference, user selected culture as fallback
--- BootstrapMvcSample/Helpers/GlobalizationHelpers.cs
+++ BootstrapMvcSample/Helpers/GlobalizationHelpers.cs
@@ -10,14 +10,14 @@ namespace System.Web.Mvc
     public static class GlobalizationHelpers
     {
         /// <summary>
-        /// Adapted from Scott Hanselman's blog post: http://www.hanselman.com/blog/GlobalizationInternationalizationAndLocalizationInASPNETMVC3JavaScriptAndJQueryPart1.aspx
+        /// Taken from Scott Hanselman's blog post: http://www.hanselman.com/blog/GlobalizationInternationalizationAndLocalizationInASPNETMVC3JavaScriptAndJQueryPart1.aspx
         /// </summary>
         /// <typeparam name="t"></typeparam>
         /// <param name="htmlHelper"></param>
         /// <returns></returns>
         public static IHtmlString MetaAcceptLanguage<t>(this HtmlHelper<t> htmlHelper)
         {
-            var acceptLanguage = HttpUtility.HtmlAttributeEncode(CultureInfo.CurrentCulture.ToString()); //Using CurrentCulture rather than CurrentUICulture as I'm interested in Date / Number localisation rather than language in particular: http://stackoverflow.com/a/329041/761388
+            var acceptLanguage = HttpUtility.HtmlAttributeEncode(CultureInfo.CurrentUICulture.ToString()); 
             return new HtmlString(string.Format("<meta name=\"accept-language\" content=\"{0}\" />", acceptLanguage));
         }
 
@@ -29,7 +29,7 @@ namespace System.Web.Mvc
         /// <returns>a culture bundle that looks something like this: "~/js-culture.en-GB"</returns>
         public static string JsCultureBundle<t>(this HtmlHelper<t> htmlHelper)
         {
-            return "~/js-culture." + CultureInfo.CurrentCulture.ToString();
+            return "~/js-culture." + CultureInfo.CurrentUICulture.ToString();
         }
     }
 }
